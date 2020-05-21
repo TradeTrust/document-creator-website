2ff63c663f90f600d16f84803e1e53fe3d3e31bc
863303 <a863303@ambsg000076.lan>
2020-05-21 11:12:38 +0800

    chore: revert file back to previous state

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -1,10 +1,9 @@
-import styled from "@emotion/styled";
 import React, { FunctionComponent } from "react";
 
-export const Container: FunctionComponent = styled(({ children }) => {
+export const Container: FunctionComponent = ({ children }) => {
   return (
     <div className="bg-secondary min-h-screen">
       <div className="container mx-auto">{children}</div>
     </div>
   );
-})``;
+};
