a02cec37530570cf43c7fee943c14f3241a8e07d
Sim Boon Long <simboonlong@gmail.com>
2020-06-12 10:12:25 +0800

    test: e2e

--- src/UI/Title/Title.tsx
+++ src/UI/Title/Title.tsx
@@ -2,7 +2,7 @@ import styled from "@emotion/styled";
 import tw from "twin.macro";
 import { mixin } from "../../styles";
 
-export const Title = styled.div`
+export const Title = styled.h1`
   ${mixin.fontRobotoBold}
   ${mixin.fontSize(28)}
   ${tw`text-grey-dark mb-8`}
