62a195936836efa17aef85643e7fc6d4af8198e4
Raymond Yeh <ray@geek.sg>
2020-07-21 17:42:43 +0800

    chore: change to extend

--- src/utils/logger.tsx
+++ src/utils/logger.tsx
@@ -1,17 +1,19 @@
 import debug from "debug";
 
-// not using .extends because of stupid next.js resolve modules bug where its picking up old version of debug
-export const trace = (namespace: string): debug.Debugger => debug(`creator:trace:${namespace}`);
-export const error = (namespace: string): debug.Debugger => debug(`creator:error:${namespace}`);
-export const stack = (namespace: string) => (error: Error): void => {
-  debug(`creator:error:${namespace}`)(error.message);
-  debug(`creator:error:${namespace}`)(error.stack);
+const creator = debug("creator");
+
+const trace = creator.extend("trace");
+const error = creator.extend("error");
+
+export const stack = (namespace: string) => (err: Error): void => {
+  error.extend(namespace)(err.message);
+  error.extend(namespace)(err.stack);
 };
 
 export const getLogger = (
   namespace: string
 ): { trace: debug.Debugger; error: debug.Debugger; stack: (error: Error) => void } => ({
-  trace: trace(namespace),
-  error: error(namespace),
+  trace: trace.extend(namespace),
+  error: error.extend(namespace),
   stack: stack(namespace),
 });
