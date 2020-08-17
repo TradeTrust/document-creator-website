0633659ee523ca7c5883e86d54e5d0246b6d978e
Raymond Yeh <ray@geek.sg>
2020-08-17 14:31:03 +0800

    chore: integration test for document preview

---
+++ integration/document-preview.spec.ts
@@ -0,0 +1,42 @@
+import { Selector } from "testcafe";
+
+fixture("Document Creator").page`http://localhost:3000`;
+
+const Config = "./../src/test/fixtures/sample-local-config.json";
+const DataFile = "./../src/test/fixtures/sample-data-file.json";
+const Title = Selector("h1");
+const Button = Selector("button");
+const ButtonLogin = Selector("[data-testid='login-button']");
+const PasswordField = Selector("[data-testid='password-field']");
+const ProgressBar = Selector("[data-testid='progress-bar']");
+const Iframe = Selector("#iframe[title='Decentralised Rendered Certificate']");
+const IframeRoot = Selector("#root");
+
+test("Upload configuration file, choose form, fill form, preview form, submit form correctly", async (t) => {
+  // upload config and reset config file
+  await t.setFilesToUpload("input[type=file]", [Config]);
+  await t.expect(Title.textContent).contains("Login with Password");
+
+  // login to step 1
+  await t.selectText(PasswordField);
+  await t.typeText(PasswordField, "password");
+  await t.click(ButtonLogin);
+  await t.expect(Title.textContent).contains("Choose Document Type to Issue");
+  await t.expect(ProgressBar.textContent).contains("Step 1/3");
+
+  // Navigate to form
+  await t.click(Button.withText("COO"));
+  await t.expect(Title.textContent).contains("Fill and Preview Form");
+  await t.expect(ProgressBar.textContent).contains("Step 2/3");
+  await t.setFilesToUpload("input[type=file][data-testid=config-file-drop-zone]", [DataFile]);
+
+  // Set preview mode to true
+  await t.click(Selector("label.toggle-switch-label"));
+  await t.switchToIframe(Iframe);
+
+  // Check that entered data is shown
+  await t.expect(IframeRoot.textContent).contains("WBC208897");
+
+  // Check that text from template (not in data) is shown
+  await t.expect(IframeRoot.textContent).contains("Place, date and signature of authorised person");
+});
