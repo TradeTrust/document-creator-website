77acb69f39d99e49c92ce617e4d937da2a2151d2
Sim Boon Long <simboonlong@gmail.com>
2020-09-28 15:17:36 +0800

    feat: nav menu

--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -1,6 +1,42 @@
 import React, { FunctionComponent } from "react";
 import { Link } from "react-router-dom";
+import { Settings } from "react-feather";
 import logo from "./logo.svg";
+
+const urlPath = process.env.NODE_ENV === "development" ? "//dev.tradetrust.io" : "//tradetrust.io";
+
+const navItems = [
+  {
+    id: "verify-documents",
+    label: "Verify Documents",
+    path: urlPath + "/#verify-documents",
+  },
+  {
+    id: "create-documents",
+    label: "Create Documents",
+    path: "/",
+  },
+  {
+    id: "resources",
+    label: "Resources",
+    path: urlPath + "/#", // to update once resources page is in
+  },
+  {
+    id: "faq",
+    label: "FAQ",
+    path: urlPath + "/faq",
+  },
+  {
+    id: "contact",
+    label: "Contact",
+    path: urlPath + "/#contact",
+  },
+  {
+    id: "settings",
+    label: "Settings",
+    path: urlPath + "/settings",
+  },
+];
 
 interface NavigationBar {
   logout?: () => void;
@@ -8,15 +44,41 @@ interface NavigationBar {
 
 export const NavigationBar: FunctionComponent<NavigationBar> = ({ logout }) => {
   return (
-    <div className="container mx-auto h-16 py-2 flex items-center justify-between">
-      <Link to="/" className="h-full">
-        <img className="h-full" src={logo} alt="TradeTrust Logo" />
-      </Link>
-      {logout && (
-        <div className="cursor-pointer" onClick={logout}>
-          Logout
+    <div className="bg-navy py-6">
+      <div className="container mx-auto">
+        <div className="flex flex-wrap items-center justify-between">
+          <div className="w-full lg:w-auto">
+            <Link to="/">
+              <img className="h-12" src={logo} alt="TradeTrust Logo" />
+            </Link>
+          </div>
+          {navItems.map((item, index) => {
+            return (
+              <div
+                className={`w-auto ml-0 pr-4 pt-4 lg:pl-8 lg:pr-0 lg:py-0 ${
+                  index === 0 ? "lg:ml-auto" : ""
+                } ${index === navItems.length ? "ml-auto" : "pr-0"}`}
+                key={index}
+              >
+                {item.path === "/" ? (
+                  <Link className="text-greyblue hover:text-white" to={item.path}>
+                    {item.label}
+                  </Link>
+                ) : (
+                  <a href={item.path} className="text-greyblue hover:text-white">
+                    {item.id === "settings" ? <Settings /> : item.label}
+                  </a>
+                )}
+              </div>
+            );
+          })}
+          {logout && (
+            <div className="cursor-pointer" onClick={logout}>
+              Logout
+            </div>
+          )}
         </div>
-      )}
+      </div>
     </div>
   );
 };
