c9e05a92587078a58c3e4a1f51409ec6849e7fed
Sim Boon Long <simboonlong@gmail.com>
2020-09-29 16:13:58 +0800

    fix: --app-init-delay more

--- scripts/integration.sh
+++ scripts/integration.sh
@@ -1,7 +1,7 @@
 account_key=0xe82294532bcfcd8e0763ee5cef194f36f00396be59b94fb418f5f8d83140d9a7
 
 # Wait for ganache to listen on 8545
-sleep 5
+sleep 2
 
 # Deploy document store to 0x63A223E025256790E88778a01f480eBA77731D04
 ./node_modules/@govtechsg/open-attestation-cli/dist/cjs/index.js deploy document-store "My Document Store" -n local -k $account_key
@@ -12,7 +12,7 @@ sleep 5
 # Deploy title escrow creator to 0x4Bf7E4777a8D1b6EdD5F2d9b8582e2817F0B0953
 ./node_modules/@govtechsg/open-attestation-cli/dist/cjs/index.js deploy title-escrow-creator "test" -n local -k $account_key
 
-if [ -z "$HEADLESS" ]
+if [  -z "$HEADLESS" ]
 then
     npm run integration:internal
 else
