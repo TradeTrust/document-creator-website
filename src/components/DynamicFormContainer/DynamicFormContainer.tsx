a6ff4df0677f035a9a503ef2e324aa2c6f6a2b80
863303 <a863303@ambsg000076.lan>
2020-10-13 15:51:35 +0800

    fix: update formData persisting bug and update token registry to v2 in sample-config

--- src/components/DynamicFormContainer/DynamicFormContainer.tsx
+++ src/components/DynamicFormContainer/DynamicFormContainer.tsx
@@ -1,12 +1,20 @@
 import React, { FunctionComponent } from "react";
 import { Redirect } from "react-router";
 import { useConfigContext } from "../../common/context/config";
+import { useFormsContext } from "../../common/context/forms";
 import { NavigationBar } from "../NavigationBar";
 import { DynamicFormLayout } from "./DynamicFormLayout";
 
 export const DynamicFormContainer: FunctionComponent = () => {
   const { config, setConfig } = useConfigContext();
-  const logout = (): void => setConfig(undefined);
+  const { setForms, setActiveFormIndex } = useFormsContext();
+
+  const logout = (): void => {
+    setForms([]);
+    setActiveFormIndex(undefined);
+    setConfig(undefined);
+  };
+
   if (!config) {
     return <Redirect to="/" />;
   }
