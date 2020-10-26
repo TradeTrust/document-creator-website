b0657ab7208ecf3ad56a1af671d5a0464f153e07
Goi Jia Jian <jiajiannn@gmail.com>
2020-10-26 17:26:02 +0800

    chore: add back uiSchema in FormData type

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -46,6 +46,7 @@ export interface FileUploadType {
 // FormData is used by json-schema-forms internally to track state of a single form
 export interface FormData {
   schema?: any; // eslint-disable-line @typescript-eslint/no-explicit-any
+  uiSchema?: any; // eslint-disable-line @typescript-eslint/no-explicit-any
   idSchema?: any; // eslint-disable-line @typescript-eslint/no-explicit-any
   formData: any; // eslint-disable-line @typescript-eslint/no-explicit-any
   edit?: boolean;
