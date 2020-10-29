7ff35395ce7c976443c9b319624dbd4d2f60a5ff
Justus Soh <45728710+justussoh@users.noreply.github.com>
2020-10-29 15:34:47 +0800

    fix: standardise qr uri to have no trailing slash (#69)
    * fix: standardise qr uri to have no trailing slash
    
    * fix: added repo field to package.json
    
    * chore: added test to assert trailing slash doesnt matter

--- src/common/utils.test.tsx
+++ src/common/utils.test.tsx
@@ -13,18 +13,22 @@ describe("encodeQrCode", () => {
     };
 
     expect(encodeQrCode(qrCode)).toStrictEqual(
-      "https://action.openattestation.com/?q=%7B%22type%22%3A%22url%22%2C%22payload%22%3A%7B%22uri%22%3A%22https%3A%2F%2Ftesturl.com%22%2C%22key%22%3A%22123%22%2C%22permittedActions%22%3A%5B%22STORE%22%5D%2C%22redirect%22%3A%22test%20Site%22%7D%7D"
+      "https://action.openattestation.com?q=%7B%22type%22%3A%22url%22%2C%22payload%22%3A%7B%22uri%22%3A%22https%3A%2F%2Ftesturl.com%22%2C%22key%22%3A%22123%22%2C%22permittedActions%22%3A%5B%22STORE%22%5D%2C%22redirect%22%3A%22test%20Site%22%7D%7D"
     );
   });
 });
 
 describe("decodeQrCode", () => {
-  it("decodes an action correctly", () => {
-    const encodedQrCode =
+  it("decodes an action correctly regardless of trailing slash", () => {
+    const encodedQrCodeSlash =
       "https://action.openattestation.com/?q=%7B%22uri%22%3A%22https%3A%2F%2Fsample.domain%2Fdocument%2Fid%3Fq%3Dabc%23123%22%7D";
+    const encodedQrCodeNoSlash =
+      "https://action.openattestation.com?q=%7B%22uri%22%3A%22https%3A%2F%2Fsample.domain%2Fdocument%2Fid%3Fq%3Dabc%23123%22%7D";
 
-    const action = decodeQrCode(encodedQrCode);
-    expect(action).toStrictEqual({
+    expect(decodeQrCode(encodedQrCodeSlash)).toStrictEqual({
+      uri: "https://sample.domain/document/id?q=abc#123",
+    });
+    expect(decodeQrCode(encodedQrCodeNoSlash)).toStrictEqual({
       uri: "https://sample.domain/document/id?q=abc#123",
     });
   });
