6aefbfc4e52ea3c1c39f5c3a1d05b77ce52c49fa
Matthea Loo <dhemutton@users.noreply.github.com>
2020-11-02 15:54:04 +0800

    feat: nav-bar navigation to verify and tests

---
+++ src/components/NavigationBar/NavigationBar.test.tsx
@@ -0,0 +1,92 @@
+import { render, screen } from "@testing-library/react";
+import React from "react";
+import { NavigationBar } from "./NavigationBar";
+import { BrowserRouter, Route } from "react-router-dom";
+import rinkebyConfig from "../../test/fixtures/sample-rinkeby-config.json";
+import ropstenConfig from "../../test/fixtures/sample-config.json";
+
+const mockMainnet = {
+  network: "homestead",
+};
+
+describe("navigationBar", () => {
+  it("should render the UI accordingly", () => {
+    render(
+      <BrowserRouter>
+        <Route exact path="/">
+          <NavigationBar logout={() => {}} />
+        </Route>
+      </BrowserRouter>
+    );
+
+    expect(screen.queryAllByText("Verify Documents")).not.toBeNull();
+    expect(screen.queryAllByText("Create Documents")).not.toBeNull();
+    expect(screen.queryAllByText("Resources")).not.toBeNull();
+    expect(screen.queryAllByText("FAQ")).not.toBeNull();
+    expect(screen.queryAllByText("Contact")).not.toBeNull();
+    expect(screen.queryByTestId("settings-icon")).not.toBeNull();
+  });
+
+  it("should go to default verify documents page without config", () => {
+    render(
+      <BrowserRouter>
+        <Route exact path="/">
+          <NavigationBar logout={() => {}} />
+        </Route>
+      </BrowserRouter>
+    );
+
+    expect(screen.getByText("Verify Documents").closest("a")).toHaveAttribute(
+      "href",
+      "https://tradetrust.io/#verify-documents"
+    );
+  });
+
+  it("should go to rinkeby verify documents page with rinkeby config", () => {
+    window.localStorage.setItem("CONFIG_FILE", JSON.stringify(rinkebyConfig));
+    render(
+      <BrowserRouter>
+        <Route path="/">
+          <NavigationBar logout={() => {}} />
+        </Route>
+      </BrowserRouter>
+    );
+
+    expect(screen.getByText("Verify Documents").closest("a")).toHaveAttribute(
+      "href",
+      "https://rinkeby.tradetrust.io/#verify-documents"
+    );
+  });
+
+  it("should go to ropsten verify documents page with ropsten config", () => {
+    window.localStorage.setItem("CONFIG_FILE", JSON.stringify(ropstenConfig));
+    render(
+      <BrowserRouter>
+        <Route path="/">
+          <NavigationBar logout={() => {}} />
+        </Route>
+      </BrowserRouter>
+    );
+
+    expect(screen.getByText("Verify Documents").closest("a")).toHaveAttribute(
+      "href",
+      "https://dev.tradetrust.io/#verify-documents"
+    );
+  });
+
+  it("should go to mainnet verify documents page with homestead config", () => {
+    window.localStorage.setItem("CONFIG_FILE", JSON.stringify(mockMainnet));
+    render(
+      <BrowserRouter>
+        <Route path="/">
+          <NavigationBar logout={() => {}} />
+        </Route>
+      </BrowserRouter>
+    );
+
+    expect(screen.getByText("Verify Documents").closest("a")).toHaveAttribute(
+      "href",
+      "https://tradetrust.io/#verify-documents"
+    );
+  });
+});
