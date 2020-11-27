3e0b3a386a984f729b892b7a059e9da759745937
Sim Boon Long <simboonlong@gmail.com>
2020-11-27 14:49:59 +0800

    chore: addons were automatically registered already

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,2 @@
 import "../src/index.css";
 import "./storybook.css";
-import { addDecorator } from "@storybook/react";
-import { withKnobs } from "@storybook/addon-knobs";
-
-addDecorator(withKnobs);
