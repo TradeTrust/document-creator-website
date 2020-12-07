c4bf923f12e41a43d179e9a4955af86822cda671
Sim Boon Long <simboonlong@gmail.com>
2020-12-07 16:58:03 +0800

    fix: rm wrapper

--- src/components/AddressResolverContainer/AddressResolverContainer.tsx
+++ src/components/AddressResolverContainer/AddressResolverContainer.tsx
@@ -1,15 +1,12 @@
 import { AddressResolver } from "@govtechsg/tradetrust-ui-components";
 import React, { FunctionComponent } from "react";
 import { NavigationBar } from "../NavigationBar";
-import { Wrapper } from "../UI/Wrapper";
 
 export const AddressResolverContainer: FunctionComponent = () => {
   return (
     <>
       <NavigationBar />
-      <Wrapper>
-        <AddressResolver />
-      </Wrapper>
+      <AddressResolver />
     </>
   );
 };
