41b3284432ba946038ec1807d7d5dcf28bd92104
Sim Boon Long <simboonlong@gmail.com>
2020-12-18 17:30:56 +0800

    fix: local dev

--- config-overrides.js
+++ config-overrides.js
@@ -2,6 +2,8 @@ const path = require("path");
 
 module.exports = function override(config) {
   config.resolve.alias.react = path.resolve("./node_modules/react");
-  config.devtool = process.env.NODE_ENV === "production" ? false : "eval-source-map"; // https://webpack.js.org/configuration/devtool/#devtool
+  if (process.env.NODE_ENV === "production") {
+    config.devtool = false; // https://webpack.js.org/configuration/devtool/#devtool
+  }
   return config;
 };
