8dc4ab163ad94d2ac60a0514c35410701732b5fc
Sim Boon Long <simboonlong@gmail.com>
2021-02-16 09:05:19 +0800

    chore: split e2e (#102)

---
+++ integration/document-attachments.spec.ts
@@ -0,0 +1,38 @@
+import { Selector } from "testcafe";
+import { enterPassword, loadConfigFile } from "./helper";
+
+fixture("Document attachments").page`http://localhost:3000`;
+
+const Config = "./../src/test/fixtures/sample-config-local.json";
+const AttachmentSample = "./../src/test/fixtures/sample.pdf";
+const Title = Selector("h1");
+const ProgressBar = Selector("[data-testid='progress-bar']");
+
+const Button = Selector("button");
+const AttachmentXButton = Selector("[data-testid='remove-uploaded-file-0']");
+const FormAttachmentField = Selector("[data-testid='upload-file-0']");
+const FormAttachmentFields = Selector("[data-testid*='upload-file-']");
+
+test("should be added and removed correctly", async (t) => {
+  // Upload config file
+  await loadConfigFile(Config);
+  await t.expect(Title.textContent).contains("Create Document");
+
+  // Login to step 1
+  await enterPassword("password");
+  await t.expect(Title.textContent).contains("Choose Document Type to Issue");
+  await t.expect(ProgressBar.textContent).contains("Step 1/3");
+
+  // Navigate to form
+  await t.click(Button.withText("Covering Letter"));
+  await t.expect(Title.textContent).contains("Fill and Preview Form");
+  await t.expect(ProgressBar.textContent).contains("Step 2/3");
+
+  // Add attachment
+  await t.setFilesToUpload("input[data-testid='attachment-file-drop-zone']", [AttachmentSample]);
+  await t.expect(FormAttachmentField.textContent).contains("sample.pdf");
+  await t.expect(FormAttachmentFields.count).eql(1);
+
+  // Remove attachment
+  await t.click(AttachmentXButton);
+});
--- integration/helper.ts
+++ integration/helper.ts
@@ -3,11 +3,15 @@ import { Selector, t } from "testcafe";
 const PasswordField = Selector("[data-testid='password-field']");
 const ButtonLogin = Selector("[data-testid='login-button']");
 
-export const loadConfigFile = async (configFile) => {
+export const loadConfigFile = async (configFile: string): Promise<void> => {
   await t.setFilesToUpload("input[type=file][data-testid=config-file-drop-zone]", [configFile]);
 };
 
-export const enterPassword = async (password) => {
+export const enterPassword = async (password: string): Promise<void> => {
   await t.typeText(PasswordField, password);
   await t.click(ButtonLogin);
+};
+
+export const deletePassword = async (): Promise<void> => {
+  await t.selectText(PasswordField).pressKey("delete");
 };
