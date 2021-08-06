534a8079b4666d6740576a6c489f350b049cb618
isaac <isaac.kps@gmail.com>
2021-08-06 14:15:25 +0800

    fix: update revoking after rebasing master

--- src/services/revoking/revoking.ts
+++ src/services/revoking/revoking.ts
@@ -1,11 +1,11 @@
 import { Wallet } from "ethers";
-import { RevokingJob } from "../../types";
+import { RevokingJob, ConnectedSigner } from "../../types";
 import { assertAddressIsSmartContract, getConnectedDocumentStore } from "../publishing";
 
-export const revokeDocumentJob = async (job: RevokingJob, wallet: Wallet): Promise<string> => {
+export const revokeDocumentJob = async (job: RevokingJob, account: Wallet | ConnectedSigner): Promise<string> => {
   const { contractAddress, targetHash, nonce } = job;
-  await assertAddressIsSmartContract(contractAddress, wallet);
-  const documentStore = await getConnectedDocumentStore(wallet, contractAddress);
+  await assertAddressIsSmartContract(contractAddress, account.provider);
+  const documentStore = await getConnectedDocumentStore(account, contractAddress);
   const receipt = await documentStore.revoke(`0x${targetHash}`, { nonce });
   const tx = await receipt.wait();
   if (!tx.transactionHash) throw new Error(`Tx hash not available: ${JSON.stringify(tx)}`);
