0d1ed495627e14c0d819bc9f3ca8ef0984373ae2
Wayne Wee <wwayne.wee@gmail.com>
2021-08-26 10:04:42 +0800

    feat: added footer

---
+++ src/constants/Urls.tsx
@@ -0,0 +1,7 @@
+export enum URLS {
+  CREATOR = "https://creator.tradetrust.io/",
+  GITHUB = "https://github.com/TradeTrust/tradetrust-website",
+  DOCS = "https://docs.tradetrust.io/",
+  MAIN = "https://tradetrust.io",
+  DEV = "https://dev.tradetrust.io",
+}
