b5581e059aafb6e14f19dc41e37610ff52a37bcb
Wayne Wee <wwayne.wee@gmail.com>
2021-09-01 16:48:00 +0800

    feat: finalised choose document page ui

--- src/components/FormSelectionContainer/FormSelection/FormSelection.tsx
+++ src/components/FormSelectionContainer/FormSelection/FormSelection.tsx
@@ -4,7 +4,7 @@ import { Redirect } from "react-router-dom";
 import { useFormsContext } from "../../../common/context/forms";
 import { Config, FormTemplate } from "../../../types";
 import { ProgressBar } from "../../ProgressBar";
-import { Title } from "../../UI/Title";
+import { SecondaryTitle } from "../../UI/Title";
 import { Wrapper } from "../../UI/Wrapper";
 import { IssueOrRevokeSelector } from "../../UI/IssueOrRevokeSelector";
 import { Card } from "../../UI/Card";
@@ -30,17 +30,15 @@ export const FormSelection: FunctionComponent<FormSelection> = ({ config }) => {
       <ContentFrame>
         <Card>
           <ProgressBar step={1} totalSteps={3} title="Choose Type" />
-          <Title className="mb-8">Choose Document Type to Issue</Title>
+          <SecondaryTitle className="my-8 font-normal">Choose Document Type to Issue</SecondaryTitle>
           <div className="flex flex-wrap justify-start">
             {config.forms.map((form: FormTemplate, index: number) => {
               return (
-                <Button
-                  className="bg-white text-cerulean hover:bg-gray-50 w-40 mb-4 mr-4"
-                  key={index}
-                  onClick={() => selectedForm(index)}
-                >
-                  {form.name}
-                </Button>
+                <div key={index} className="bg-white hover:bg-cloud-100 w-1/3 mb-4">
+                  <Button className="text-cerulean w-11/12 h-full p-4" onClick={() => selectedForm(index)}>
+                    {form.name}
+                  </Button>
+                </div>
               );
             })}
           </div>
--- src/components/UI/Title/Title.tsx
+++ src/components/UI/Title/Title.tsx
@@ -5,5 +5,10 @@ import { mixin } from "../../../styles";
 export const Title = styled.h1`
   ${mixin.fontRobotoBold}
   ${mixin.fontSize(28)}
-  ${tw`text-gray-800`}
+  ${tw`text-cloud-900`}
+`;
+
+export const SecondaryTitle = styled.h1`
+  ${mixin.fontSize(24)}
+  ${tw`text-cloud-900`}
 `;
