1b536a7392a0303bb64eb3be1344286376ff0eec
Wayne Wee <wwayne.wee@gmail.com>
2021-09-01 16:48:00 +0800

    feat: finalised choose document page ui

--- src/components/UI/IssueOrRevokeSelector/IssueOrRevokeSelector.tsx
+++ src/components/UI/IssueOrRevokeSelector/IssueOrRevokeSelector.tsx
@@ -2,11 +2,11 @@ import { FunctionComponent } from "react";
 import { NavLink } from "react-router-dom";
 
 export const IssueOrRevokeSelector: FunctionComponent = () => (
-  <div className="space-x-6 text-cloud-900 text-lg font-medium mb-4">
-    <NavLink to="/forms-selection" activeClassName="text-blue underline">
-      Issue Document
+  <div className="space-x-6 text-cloud-900 text-lg mb-4">
+    <NavLink to="/forms-selection" activeClassName="font-bold text-blue underline">
+      Create Document
     </NavLink>
-    <NavLink to="/revoke" data-testid="choose-revoke-button" activeClassName="text-blue underline">
+    <NavLink to="/revoke" data-testid="choose-revoke-button" activeClassName="font-bold text-blue underline">
       Revoke Document
     </NavLink>
   </div>
