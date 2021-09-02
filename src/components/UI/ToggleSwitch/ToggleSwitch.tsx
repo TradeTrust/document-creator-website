de29aa038784060fb0e02496f513ac704cee81bf
Wayne Wee <wwayne.wee@gmail.com>
2021-09-02 16:39:55 +0800

    chore: removed unnecessary div

--- src/components/UI/ToggleSwitch/ToggleSwitch.tsx
+++ src/components/UI/ToggleSwitch/ToggleSwitch.tsx
@@ -7,29 +7,27 @@ interface ToggleSwitchProps {
 
 export const ToggleSwitch: FunctionComponent<ToggleSwitchProps> = ({ isOn, handleToggle }) => {
   return (
-    <div className="">
-      <label data-testid="toggle-switch-label" className="flex items-center cursor-pointer">
-        <div className="relative">
-          <input
-            checked={isOn}
-            className="hidden"
-            onChange={handleToggle}
-            type="checkbox"
-            id="toggle-switch"
-            data-testid="toggle-switch"
-          />
-          <div className={`block ${isOn ? `bg-cerulean` : `bg-cloud-200`} w-16 h-8 rounded-lg`} />
-          <div
-            className={`w-full absolute top-1 transform duration-100 ${
-              isOn ? `right-2 translate-x-2/3` : `left-1 translate-x-0`
-            }`}
-          >
-            <div className={`dot rounded-lg bg-white w-6 h-6`} />
-          </div>
-          <div className={`text-sm absolute top-1.5 right-1 text-white${isOn ? " hidden" : ""}`}>OFF</div>
-          <div className={`text-sm absolute top-1.5 left-1 text-white${isOn ? "" : " hidden"}`}>ON</div>
+    <label data-testid="toggle-switch-label" className="flex items-center cursor-pointer">
+      <div className="relative">
+        <input
+          checked={isOn}
+          className="hidden"
+          onChange={handleToggle}
+          type="checkbox"
+          id="toggle-switch"
+          data-testid="toggle-switch"
+        />
+        <div className={`block ${isOn ? `bg-cerulean` : `bg-cloud-200`} w-16 h-8 rounded-lg`} />
+        <div
+          className={`w-full absolute top-1 transform duration-100 ${
+            isOn ? `right-2 translate-x-2/3` : `left-1 translate-x-0`
+          }`}
+        >
+          <div className={`dot rounded-lg bg-white w-6 h-6`} />
         </div>
-      </label>
-    </div>
+        <div className={`text-sm absolute top-1.5 right-1 text-white${isOn ? " hidden" : ""}`}>OFF</div>
+        <div className={`text-sm absolute top-1.5 left-1 text-white${isOn ? "" : " hidden"}`}>ON</div>
+      </div>
+    </label>
   );
 };
