99ac1a80350fc2d59ab906dded232fbfbb5ef1ef
Wayne Wee <wwayne.wee@gmail.com>
2021-09-07 17:46:40 +0800

    feat: update process page ui

--- src/components/ProcessDocumentScreen/ProcessDocumentScreen.tsx
+++ src/components/ProcessDocumentScreen/ProcessDocumentScreen.tsx
@@ -103,12 +103,15 @@ export const ProcessDocumentScreen: FunctionComponent<ProcessDocumentScreen> = (
             {failPublishedDocuments && failPublishedDocuments.length > 0 && queueState !== QueueState.ERROR && (
               <>
                 <ErrorCard
-                  title={`The document(s) could not be ${isIssuingFlow ? "issued" : "revoked"} at this time.`}
-                  description={`Please contact TradeTrust via email or client representative to resolve your issue. Alternatively, please try again.`}
-                  buttonProps={{
-                    onClick: () => processDocuments(type),
-                    text: "Try Again",
-                  }}
+                  title={`The document(s) could not be ${isIssuingFlow ? "published" : "revoked"} at this time.`}
+                  description={
+                    "Please contact TradeTrust via email or client representative to resolve your issue. Alternatively, please try again."
+                  }
+                  buttonLink={
+                    <a className="text-cerulean" onClick={() => processDocuments(type)}>
+                      Try Again
+                    </a>
+                  }
                 />
 
                 <div className="py-3">
