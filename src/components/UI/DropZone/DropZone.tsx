4003df162c8629d5afa40864c18c5cba113118d6
ChenRen <siachenren@gmail.com>
2021-10-18 12:55:54 +0800

    fix: lint error

--- src/components/UI/DropZone/DropZone.tsx
+++ src/components/UI/DropZone/DropZone.tsx
@@ -11,7 +11,7 @@ export const DropZone: FunctionComponent<DropZoneProps> = ({ children, isDragAct
   let dropZoneCss = `cursor-pointer rounded-xl border-dashed border-2 border-cloud-100 items-center flex flex-col pt-16 pb-16 px-4 text-center`;
 
   dropZoneCss += error ? ` bg-red-100` : ` bg-white`;
-  
+
   dropZoneCss += error ? (isDragActive ? ` bg-gray-300` : ` bg-red-100`) : isDragActive ? ` bg-gray-300` : ` bg-white`;
 
   return <div className={customDropZoneCss ?? dropZoneCss}>{children}</div>;
