6368cea8990089bd6ba0a83d8f6002cc3d766281
Sim Boon Long <simboonlong@gmail.com>
2021-11-26 14:57:51 +0800

    chore: config cleanup

--- src/test/fixtures/config/types.ts
+++ src/test/fixtures/config/types.ts
@@ -1,10 +1,7 @@
 import { v2 } from "@govtechsg/open-attestation";
 import { DocumentStorage, Network, WalletOptions } from "../../../types";
-
-// version property is used to differentiate the
-// placement of the credential based on their respective schema
 export interface Forms {
-  version: "v2" | "v3" | "v2-did" | "v3-did";
+  version: "v2" | "v3" | "v2-did" | "v3-did"; // version property is used to differentiate the placement of the credential based on their respective schema
   forms: any;
 }
 
@@ -33,12 +30,10 @@ export interface EmptyConfig {
   documentStorage?: DocumentStorage;
 }
 
-// validationBypass is used for local configuration to
-// create error configuration used for testing purposes
 export interface GenerateConfigFileProps {
   configFile: EmptyConfig;
   formsList: Forms[];
   credential: Credential | DidCredential;
   directory: string;
-  validationBypass?: boolean;
+  validationBypass?: boolean; // validationBypass is used for local configuration to create error configuration used for testing purposes
 }
--- src/test/fixtures/forms/v3/index.tsx
+++
@@ -1,4 +0,0 @@
-import BillOfLading from "./bill-of-lading.json";
-import CooChafta from "./coo-chafta.json";
-import Invoice from "./invoice.json";
-export { BillOfLading, CooChafta, Invoice };
