b08835e2942bc18212789a40c03466c28ce2eaf6
BlocksScanIO <support@blocksscan.io>
2022-12-02 21:51:16 +0530

    XDC Network details added.

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -3,7 +3,7 @@ import { OpenAttestationDocument } from "@govtechsg/open-attestation";
 import { ErrorObject } from "ajv";
 import { Signer, Wallet } from "ethers";
 
-export type Network = "homestead" | "goerli" | "local" | "sepolia" | "matic" | "maticmum";
+export type Network = "homestead" | "goerli" | "local" | "sepolia" | "matic" | "maticmum" | "xdc" | "xdcapothem";
 type FormType = "TRANSFERABLE_RECORD" | "VERIFIABLE_DOCUMENT";
 
 // FormTemplate is defined in configuration file
