821f0bfd62c5f78e098b538cbd12807d04f04bc9
Puay Hiang <68947167+puayhiang@users.noreply.github.com>
2023-02-28 17:10:08 +0800

    feat: replace ganache db with local goerli

--- scripts/setup-contracts.js
+++ scripts/setup-contracts.js
@@ -5,7 +5,7 @@ const ACCOUNT_KEY = "0xe82294532bcfcd8e0763ee5cef194f36f00396be59b94fb418f5f8d83
 // Deploy document store to 0x4Bf7E4777a8D1b6EdD5F2d9b8582e2817F0B0953
 shell.exec(`oa deploy document-store "My Document Store" -n local -k ${ACCOUNT_KEY}`);
 
-// Deploy token registry to 0x9Eb613a88534E2939518f4ffBFE65F5969b491FF
+// Deployed token registry at 0xf18CD26780B6D3589371fb0b3fE8E2a513D6Fdc6 on Goerli
 // shell.exec(`oa deploy token-registry "My Token Registry" MTR -n local -k ${ACCOUNT_KEY}`);
 
 // Issue a verifiable document for revoke flow
