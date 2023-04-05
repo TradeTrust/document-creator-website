1998787cd51362606380a6e48d411d5f5328ca59
isaackps <isaac.kps@gmail.com>
2023-04-05 10:29:03 +0800

    fix: update component naming

--- src/components/FooterBar/FooterBar.tsx
+++ src/components/FooterBar/FooterBar.tsx
@@ -1,44 +1,9 @@
-import { Footer, FooterColumnItemProps } from "@govtechsg/tradetrust-ui-components";
+import { Footer } from "@govtechsg/tradetrust-ui-components";
 import { FunctionComponent } from "react";
-import { ExternalLink } from "react-feather";
-import { NavLink } from "react-router-dom";
 import { usePersistedConfigFile } from "../../common/hook/usePersistedConfigFile";
 import { URLS } from "../../constants/Urls";
 import { getNetworkPath } from "../../utils";
-
-const sharedStyles = `text-sm text-cloud-500 hover:text-cerulean-500`;
-
-interface NavLinkInterface {
-  (props: FooterColumnItemProps): JSX.Element;
-}
-
-const renderNavLink: NavLinkInterface = ({ label, to }) => {
-  return (
-    <NavLink className={sharedStyles} to={to}>
-      {label}
-    </NavLink>
-  );
-};
-
-// This component is not being used at the moment, but if you ever want an external link without the icon please use this component.
-// const renderExternalLinkWithoutIcon = ({ label, to }: FooterColumnItemProps) => {
-//   return (
-//     <a className="flex items-center" href={to} target={"_blank"} rel="noopener noreferrer">
-//       <p className={`${sharedStyles} mr-1`}>{label}</p>
-//     </a>
-//   );
-// };
-
-const renderExternalLink: NavLinkInterface = ({ label, to }) => {
-  return (
-    <a className="flex items-center" href={to} target={"_blank"} rel="noopener noreferrer">
-      <p className={`${sharedStyles} mr-1`}>{label}</p>
-      <div className="w-auto text-cloud-400">
-        <ExternalLink size={12} />
-      </div>
-    </a>
-  );
-};
+import { NavLinkInterface, RenderExternalLink, RenderNavLink, BottomRenderExternalLink } from "./FooterBarLink";
 
 interface footerData {
   category: string;
@@ -60,56 +25,50 @@ const getData: GetDataInterface = (networkPath: string) => {
     {
       category: "Utilities",
       items: [
-        { label: "Verify Documents", to: `${networkPath}/verify`, render: renderExternalLink },
-        { label: "Create Documents", to: "/", render: renderNavLink },
+        { label: "Verify Documents", to: `${networkPath}/verify`, render: RenderExternalLink },
+        { label: "Create Documents", to: "/", render: RenderNavLink },
       ],
     },
     {
       category: "Resources",
       items: [
-        { label: "Learn", to: `${networkPath}/learn`, render: renderExternalLink },
-        { label: "FAQ", to: `${networkPath}/faq`, render: renderExternalLink },
-        { label: "ETA", to: `${networkPath}/eta`, render: renderExternalLink },
+        { label: "Learn", to: `${networkPath}/learn`, render: RenderExternalLink },
+        { label: "FAQ", to: `${networkPath}/faq`, render: RenderExternalLink },
+        { label: "ETA", to: `${networkPath}/eta`, render: RenderExternalLink },
       ],
     },
     {
       category: "News & Event",
       items: [
-        { label: "News", to: `${networkPath}/news`, render: renderExternalLink },
-        { label: "Events", to: `${networkPath}/events`, render: renderExternalLink },
+        { label: "News", to: `${networkPath}/news`, render: RenderExternalLink },
+        { label: "Events", to: `${networkPath}/events`, render: RenderExternalLink },
       ],
     },
     {
       category: "Support",
       items: [
-        { label: "Github", to: URLS.GITHUB, render: renderExternalLink },
-        { label: "Documentation", to: URLS.DOCS, render: renderExternalLink },
-        { label: "Contact", to: `${networkPath}/contact`, render: renderExternalLink },
-        { label: "FAQ", to: `${networkPath}/faq`, render: renderExternalLink },
+        { label: "Github", to: URLS.GITHUB, render: RenderExternalLink },
+        { label: "Documentation", to: URLS.DOCS, render: RenderExternalLink },
+        { label: "Contact", to: `${networkPath}/contact`, render: RenderExternalLink },
+        { label: "FAQ", to: `${networkPath}/faq`, render: RenderExternalLink },
       ],
     },
     {
       category: "Settings",
       items: [
-        { label: "Address Book", to: "/settings/address-book", render: renderNavLink },
-        { label: "Address Book Resolver", to: "/settings/address-resolver", render: renderNavLink },
+        { label: "Address Book", to: "/settings/address-book", render: RenderNavLink },
+        { label: "Address Book Resolver", to: "/settings/address-resolver", render: RenderNavLink },
       ],
     },
   ];
 };
 
-const bottomRender = ({ label, to }: FooterColumnItemProps): React.ReactElement => (
-  <a href={to} className="text-cloud-500 text-sm px-4 border-r">
-    {label}
-  </a>
-);
-
 const legalData = (networkPath: string) => {
   return {
     copyright: "Copyright \u00A9 2021 TradeTrust",
     items: [
-      { label: "Privacy Policy", to: `${networkPath}/privacy-policy`, render: bottomRender },
-      { label: "Terms of use", to: `${networkPath}/terms-of-use`, render: bottomRender },
+      { label: "Privacy Policy", to: `${networkPath}/privacy-policy`, render: BottomRenderExternalLink },
+      { label: "Terms of use", to: `${networkPath}/terms-of-use`, render: BottomRenderExternalLink },
     ],
   };
 };
